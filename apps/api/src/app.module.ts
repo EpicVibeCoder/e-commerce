a5658423646da3615749a42e55ead4ba615645ba
EpicVibeCoder <ashiq.racer@gmail.com>
2026-09-12 13:54:11 +0600

    refactor(api): remove GraphQL support and related dependencies
    - Deleted GraphQL module, resolvers, and input classes to streamline the API structure.
    - Removed Apollo Server and associated dependencies from package.json and package-lock.json.
    - Updated app.module.ts to eliminate GraphQL module imports, enhancing clarity and focus on core functionalities.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -11,7 +11,6 @@ import { HealthModule } from "src/health/health.module";
 import { RedisModule } from "./redis/redis.module";
 import { CategoriesModule } from "./categories/categories.module";
 import { ProductsModule } from "./products/products.module";
-import { AppGraphQLModule } from "./graphql/graphql.module";
 
 @Module({
       imports: [
@@ -27,7 +26,6 @@ import { AppGraphQLModule } from "./graphql/graphql.module";
             RedisModule,
             CategoriesModule,
             ProductsModule,
-            AppGraphQLModule,
       ],
       controllers: [AppController],
       providers: [AppService],
